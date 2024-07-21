7a45a0e96476100ada6b572331c06d993ec2a357
Zamir-zamok <ekhismatov@mail.ru>
2024-07-21 14:54:37 +0500

    Add files via upload

---
+++ modul1.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+a = sum(grades[0])/len(grades[0])
+b = sum(grades[1])/len(grades[1])
+c = sum(grades[2])/len(grades[2])
+d = sum(grades[3])/len(grades[3])
+e = sum(grades[4])/len(grades[4])
+list_ = ['Aaron','Bilbo','Johnny','Khendrik','Steve']
+estimation = { list_[0]:a,list_[1]:b,list_[2]:c,list_[3]:d,list_[4]:e, }
+print(estimation)
