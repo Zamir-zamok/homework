9889038444cda4464d06f04d7c2b106d83aaafa3
Zamir-zamok <ekhismatov@mail.ru>
2024-10-13 00:49:05 +0500

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,94 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor > self.number_of_floors or self.new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, self.new_floor + 1):
+                if i == self.new_floor:
+                    print(f'Этаж номер: {i}, Вы приехали')
+                else:
+                    print(f'Этаж номер: {i}')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+
+    def __add__(self, value):
+        if isinstance(value, House):
+            self.number_of_floors += value
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
