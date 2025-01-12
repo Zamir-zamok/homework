cdfd7f5b2e60d73876833553abb0ecaefa094ebd
Zamir-zamok <ekhismatov@mail.ru>
2025-01-12 11:35:51 +0500

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -1,14 +1,8 @@
 def add_everything_up(a, b):
     try:
-
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        if isinstance(a, (int, float, str)) and isinstance(b, (int, float, str)):
             return a + b
 
-        elif isinstance(a, str) and isinstance(b, str):
-            return a + b
-
-        else:
-            raise TypeError
     except TypeError:
 
         return f'{str(a)}{str(b)}'
@@ -16,4 +10,4 @@ def add_everything_up(a, b):
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))  
+print(add_everything_up(123.456, 7))
